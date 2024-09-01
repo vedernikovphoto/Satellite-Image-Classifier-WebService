f8d1c04d19202f12f50155e8f8bada6e27f5a8f2
vedernikovphoto <aleksandr.vedernikov@skoltech.ru>
2024-09-01 12:30:32 +0300

    Fixed unit and integration tests

--- tests/unit/services/test_planet_analysys.py
+++ tests/unit/services/test_planet_analysys.py
@@ -23,9 +23,9 @@ def test_predicts_not_fail(app_container: AppContainer, sample_image_np: np.ndar
     """
     with app_container.reset_singletons():
         with app_container.planet_classifier.override(FakePlanetClassifier()):
-            planet_analytics = app_container.planet_analytics()
-            planet_analytics.predict(sample_image_np)
-            planet_analytics.predict_proba(sample_image_np)
+            planet_classifier = app_container.planet_classifier()
+            planet_classifier.predict(sample_image_np)
+            planet_classifier.predict_proba(sample_image_np)
 
 
 def test_prob_less_or_equal_to_one(app_container: AppContainer, sample_image_np: np.ndarray):
@@ -37,8 +37,8 @@ def test_prob_less_or_equal_to_one(app_container: AppContainer, sample_image_np:
     """
     with app_container.reset_singletons():
         with app_container.planet_classifier.override(FakePlanetClassifier()):
-            planet_analytics = app_container.planet_analytics()
-            planet2prob = planet_analytics.predict_proba(sample_image_np)
+            planet_classifier = app_container.planet_classifier()
+            planet2prob = planet_classifier.predict_proba(sample_image_np)
             for planet, prob in planet2prob.items():
                 if prob < 0 or prob > 1:
                     raise AssertionError(f"Probability for planet '{planet}' is out of range: {prob}")
